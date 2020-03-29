4decdc9dd13f554ce4c1d47d0b6968f98c83ff67
Yijiao <qyj@umich.edu>
2020-03-29 04:05:41 -0400

    Modify and Explain

--- verilog/load.sv
+++ verilog/load.sv
@@ -6,7 +6,7 @@ typedef struct packed {
     logic [`SQ_LEN-1:0]     age;
     logic                   rsvd;   //  Load address is resolved
     logic                   issue;
-    logic                   no_preceding_store;
+    logic  [3:0]            load_byte; // LOAD 1 BYTE / HALF WORD/ ONE WORD
 } LB_ENTRY;
 
 typedef struct packed {
@@ -28,22 +28,21 @@ module load_buffer (
     input                                       sq_head,
     input                                       sq_tail,  
     input                                       sq_addr_all_rsvd,
-    input                                       oldest_unrsvd_sq_idx,  
-    // From D$ or SQ
-    input                                       retire_tag,
+    input                                       secure_age,  
     // To previous stage : no space for you
     output  logic                               lb_full,
     // To RS 
-    output  logic      [`LB_LEN-1:0]            asnd_lb_idx,    
+    output  logic      [`LB_LEN-1:0]            Assigned_lb_idx,    
     // To SQ
     output  logic                               lb_request_valid,
     output  LB_ENTRY                            lb_request_entry,
 );
+
     LOAD_BUFFER                                 LB;
     logic                                       lb_full;
     assign                                      lb_full = LB.free_list==0;
 
-    // Choose an entry to put the new instruction
+    // Choose a free entry to put the new instruction
     logic [`LB_LEN-1:0]                      free_idx;
     always_comb begin
         free_idx = `LB_LEN'h0; 
@@ -52,33 +51,40 @@ module load_buffer (
         end
     end
 
-    // Loads are allowed to execute out-of-order when they
-    // are not preceded by a store with an unresolved address
+    // Choose the issue_list from the resolved LB entries
+    // They should be older than oldest unresolved store instruction
     always_comb begin
         for (int j=0; j<`LB_CAPACITY; j++) begin
             if (!LB.entries[j].rsvd) 
-                LB.issue_list[j] = 0;
-            else begin
-                if (LB.entries[j].no_preceding_store || sq_addr_all_rsvd)
-                    LB.issue_list[j] = 1;
-                if (LB.entries[j].age == sq_head)
-                    LB.issue_list[j] = 1;
-                else begin
-                    if (sq_head <= oldest_unrsvd_sq_idx) begin
-                        if(LB.entries[j].age -1 <  oldest_unrsvd_sq_idx)
-                            LB.issue_list[j] = 1;
-                    end
-                    else begin
-                        if (LB.entries[j].age -1>sq_head&&LB.entries[j].age -1 < oldest_unrsvd_sq_idx)
-                            LB.issue_list[j] = 1;
-                        else
-                            LB.issue_list[j] = 0;
+                LB.issue_list[j] = 0; // Unresolved loads are not considered
+            else begin  
+                // Consider the loads older than the secure_age
+                // E.g. Index 0 - store ...
+                //                load (age: 1)
+                //      Index 1 - store ???
+                // Even if the store at index 0 retires
+                // new sq_head has index 1, which is equal the load_age 1
+                // Hence we should consider load_age geq current sq_head
+                //                                   leq secure_age
+                if (sq_head <= secure_age) begin
+                    //  START [....... |HEAD ------ SECURE ---|..... ] END
+                    //  Whether TAIL wraps doesn't matter, we only care about the range between H&S
+                    if (LB.entries[j].age >= sq_head && LB.entries[j].age <= secure_age)
+                        LB.issue_list[j] = 1;
+                end
+                else if (sq_head > secure_age) begin
+                    //  START [|---- SECURE ----| ...... |HEAD ----|] END
+                    if(LB.entries[j].age >= sq_head || LB.entries[j].age <= secure_age)
+                        LB.issue_list[j] = 1;
                     end
                 end
+                else
+                    LB.issue_list[j] = 0;
             end 
         end
     end
 
+    // Choose an issuable entry to issue with the given selector
     logic none_selected;
     logic [`LB_CAPACITY-1:0] gnt_bus;
     logic [`LB_CAPACITY-1:0] psel_gnt;
@@ -90,53 +96,51 @@ module load_buffer (
     );
     logic [`LB_LEN-1:0] issue_idx;
     always_comb begin
-        issue_idx = `LB_CAPACITY'h0; // avoid additional latching
+        issue_idx = `LB_CAPACITY'h0; 
         for (int j=0; j<`LB_CAPACITY; j++) begin
             if (psel_gnt[j]) issue_idx = j; 
         end
     end
 
+    // Sequentially 
     always_ff @(posedge clock) begin
         if (reset) begin
             for(int i=0;i<`LB_CAPACITY;i++) begin
                 LB.entries[i].rsvd <= `SD 0;
-                LB.entries[i].rsvd <= `SD 0;
-                LB.entries[i].no_preceding_store <= `SD 0;
             end 
             LB.free_list <= `SD {`LB_CAPACITY{1}};
             LB.rsvd_list <= `SD {`LB_CAPACITY{0}};
             LB.issue_list <= `SD {`LB_CAPACITY{0}};
         end
         
-        if (lb_enable) begin  // An inst enters the load buffer
+        if (lb_enable) begin  
+            // Tell RS this inst is entered into FREE_INDEX 
+            Assigned_LB_idx <= `SD free_idx;
+            // Age is the current SQ tail, new entry is always unresolved
             LB.entries[free_idx].age <= `SD sq_tail;
-            LB.free_list[free_idx] <= `SD 0;    
-            asnd_LB_idx <= `SD free_idx;
-
-            LB.rsvd_list[free_idx] <=`SD 0;
             LB.entries[free_idx].rsvd <=`SD 0;
-            if (sq_empty)
-                LB.entries[free_idx].no_preceding_store <= `SD 1;
+            // Update the list - this entry no longer free/resolved
+            LB.free_list[free_idx] <= `SD 0;    
+            LB.rsvd_list[free_idx] <=`SD 0;
         end 
         
-        // The load address is resolved
+        // RS fills information into specific entry when it's ready
         if (rs_lb_out_valid&&LB.entries[rs_lb_packet.lb_idx].rsvd==0) begin
             LB.entries[rs_lb_packet.lb_idx].rsvd <=`SD 1;
             LB.entries[rs_lb_packet.lb_idx].addr <= `SD rs_lb_packet.base_value + rs_lb_packet.offset;
             LB.entries[rs_lb_packet.lb_idx].rd_preg <=`SD rs_lb_packet.dest_preg_idx;
             LB.entries[rs_lb_packet.lb_idx].rob_idx <=`SD rs_lb_packet.rob_idx;
             LB.entries[rs_lb_packet.lb_idx].PC <=`SD rs_lb_packet.PC;
+            LB.entries[rs_lb_packet.lb_idx].load_byte <=`SD rs_lb_packet.load_byte;
         end
 
+        // Some load inst can be issued
+        // Once issued; Retire from LB
         if (!none_selected) begin
             lb_request_valid <= `SD 1;
             lb_request_entry <= `SD LB.entries[issue_idx];
+            LB.free_list[j] <=`SD 1;
         end
-
-        for (int j=0; j<`LB_CAPACITY; j++) begin
-            if (LB.entries[j].rob_idx==retire_tag) 
-                LB.free_list[j] <=`SD 1;
-        end   
     end
 
 endmodule
--- verilog/store.sv
+++ verilog/store.sv
@@ -4,6 +4,7 @@ typedef struct packed {
     logic [`XLEN-1:0]       data;
     logic                   rob_idx;
     logic                   rsvd;
+    logic  [3:0]            store_byte; // STORE 1 BYTE / HALF WORD/ ONE WORD
 } SQ_ENTRY;
 
 typedef struct packed {
@@ -17,7 +18,14 @@ typedef struct packed {
     logic [`XLEN-1:0]       forward_data;
     logic [4:0]             rd_preg;
     logic                   rob_idx;    
-} FORWARD;
+} FORWARD_PACKET;
+
+typedef struct packed {            
+	logic [`XLEN-1:0]       PC;                
+    logic [`XLEN-1:0]       addr;
+    logic [`XLEN-1:0]       data;
+    logic  [3:0]            store_byte; // STORE 1 BYTE / HALF WORD/ ONE WORD
+} SQ_2_SB_PACKET;
 
 module store_queue (
     input                               clock,
@@ -28,108 +36,116 @@ module store_queue (
     input                               rs_sq_packet,    
     // From load instruction
     input                               lb2sq_request_entry,
-    // From Dcache
-    input   [`SQ_LEN-1:0]               Dcache2SQ_response,   
-    
-    // Output to D_cache
-    output  SQ_ENTRY                    sq_head_entry,
-    output  LB_ENTRY                    lb2cache_request_entry,
+    // From rob
+    input                               retire_enable,   // Store @ROB-HEAD
+    // To ROB
+    output  logic                       sq_head_rsvd, 
     // Out to previous stage
     output  logic                       sq_full,
     // Output to load buffer
-    output  logic                       sq_all_rsvd,
     output  logic                       sq_empty,
     output  logic [`SQ_LEN-1:0]         sq_tail,         // Also to MEM_RS 
     output  logic [`SQ_LEN-1:0]         sq_head,
-    output  logic [`SQ_LEN-1:0]         oldest_unrsvd_sq_idx, 
+    output  logic [`SQ_LEN-1:0]         secure_age, 
     // To CDB
     output  logic                       forward_valid,
-    output  FORWARD                     forward_pack,
+    output  FORWARD_PACKET              forward_pack,
+    // To Store-Buffer -- Another Advanced Feature
+    output  SQ_2_SB_PACKET              sq2sb_packet,
+    // Output to D_cache
+    output logic                        lb2cache_request_valid,
+    output  LB_ENTRY                    lb2cache_request_entry,
 );
-    STORE_QUEUE                         SB;
+    STORE_QUEUE                         SQ;
     logic [`SQ_LEN:0]                   counter;
     logic                               sq_full, sq_empty;
-    logic                               match;
+    logic                               forward_match;
     logic [`XLEN-1:0]                   forward_data;
     logic [`SQ_LEN-1:0]                 sq_tail;
     logic [`SQ_LEN-1:0]                 sq_head;
-    logic [`SQ_LEN:0]                   unkwn_idx;
+
     assign sq_tail = SQ.tail;
     assign sq_head = SQ.head;
-    assign sq_full = counter==`SQ_CAPACITY;
     assign sq_empty = counter==0;
-    assign sq_head_entry = SQ.entries[head];
-
-    // The valid range is [From old to young] :
-    // If (SQ.head < SQ.tail || sq_empty)
-    // HEAD -> TAIL
-    // SQ.head >= SQ.tail
-    // HEAD -> `SQ_CAPACITY-1 -> 0 -> TAIL
+    assign sq_full = counter==`SQ_CAPACITY-1; 
+    // When SQ is full, TAIL won't overlap HEAD; To avoid some age problems
+    
+    assign sq_head_rsvd = SQ.entries[head].rsvd;
+    // Compute the secure_age [sequential version of unkwn_idx]
+    // That is the index of the oldest unresolved store instruction
+    // Told LB secure_age to help her decide which load could be issued
+    logic [`SQ_LEN-1:0]                 unkwn_idx;
     always_comb begin
-        // Compute the oldest_unrsvd_sq_idx
-        if (sq_empty)
-            unkwn_idx = `SQ_CAPACITY;
-        else begin
-            unkwn_idx = `SQ_CAPACITY;
-            if (SQ.head < SQ.tail) begin
-                for (int i=SQ.tail-1; i>=SQ.head;i--) begin
-                    if (SQ.entries[i].rsvd==0)
-                         unkwn_idx = i;
-                end
+        unkwn_idx = `SQ_CAPACITY-1;
+        // Default is the max_index
+        // Because when all addresses are resolved, we need a secure_age that's larger than any load_age
+        if (SQ.head < SQ.tail || SQ.tail==0) begin
+            for (int i=SQ.tail-1; i>=SQ.head;i--) begin
+                if (SQ.entries[i].rsvd==0)
+                        unkwn_idx = i;
             end
-            else begin
-                for (int i=SQ.tail-1; i>=0;i--) begin
-                    if (SQ.entries[i].rsvd==0)
-                         unkwn_idx = i;
-                end
-                for (int i=`SQ_CAPACITY-1; i>=SQ.head;i--) begin
-                    if (SQ.entries[i].rsvd==0)
-                         unkwn_idx = i;
+        end
+        else begin
+            for (int i=SQ.tail-1; i>=0;i--) begin
+                if (SQ.entries[i].rsvd==0)
+                        unkwn_idx = i;
+            end
+            for (int i=`SQ_CAPACITY-1; i>=SQ.head;i--) begin
+                if (SQ.entries[i].rsvd==0)
+                    unkwn_idx = i;
                 end
             end
         end
     end
 
+    // To handle the LB forward request
+    // We should find the matching address
+    // Also we need to consider such cases:
+    // STORE 1 byte in the address but the load instruction loads 1 word
+    // This is not a perfect match which needs overwritten in D$/Memory
     logic [`XLEN-1:0]       load_addr;
     logic [`SQ_LEN-1:0]     load_age;
-
+    logic [`SQ_LEN-1:0]     forward_match_idx;
     always_comb begin
         load_addr = lb2sq_request_entry.addr;
         load_age = lb2sq_request_entry.age;
-        if (!sq_full && SQ.head==SQ.tail) begin
-            match = 0;
+        forward_match = 0;
+        if (sq_empty) begin
+            forward_match = 0;
         end
-        else if (SQ.head < SQ.tail && SQ.head < load_age) begin
+        else if ((SQ.head < SQ.tail||SQ.tail==0)&& SQ.head<load_age) begin
             for (int i=SQ.head; i<=load_age-1; i++) begin
                 if (load_addr==SQ.entries[i].addr) begin
-                    match = 1;
-                    forward_data = SQ.entries[i].data;
+                    forward_match = 1;
+                    forward_match_idx = i;
                 end
             end
         end
-        else if (SQ.head >= SQ.tail && !(load_age<=SQ.head&&load_age>SQ.tail)) begin
-            int i;
-            i = (`SQ_CAPACITY>load_age)?load_age:`SQ_CAPACITY;       
-            for (i=SQ.head; (i<`SQ_CAPACITY)&&i(<load_age); i++) begin
+        else begin
+            int i;    
+            for (i=SQ.head; (i<`SQ_CAPACITY)&&(i<load_age); i++) begin
                 if (load_addr==SQ.entries[i].addr) begin
-                    match = 1;
-                    forward_data = SQ.entries[i].data;
+                    forward_match = 1;
+                    forward_match_idx = i;
                 end
             end
             for (i=0; (i<load_age)&&(i<SQ.tail); i++) begin
                 if (load_addr==SQ.entries[i].addr) begin
-                    match = 1;
-                    forward_data = SQ.entries[i].data;
+                    forward_match = 1;
+                    forward_match_idx = i;
                 end
             end
-        end
-        else
-            match = 0;
+        end   
+        if (forward_match) begin
+            if (lb2sq_request_entry.load_byte <= SQ.entries[i].store_byte)
+                forward_data = SQ.entries[i].data;
+                // Need further revision
+        end      
     end
 
-    FORWARD 4ward, forward_pack;
+    FORWARD_PACKET 4ward, forward_pack;
     always_comb begin
-        if(match) begin
+        if(forward_match) begin
             4ward.PC = lb2sq_request_entry.PC;
             4ward.forward_data <=`SD forward_data;
             4ward.rd_preg <=`SD lb2sq_request_entry.rd_preg;
@@ -145,13 +161,11 @@ module store_queue (
             for(int i=0;i<`SQ_CAPACITY;i++) begin
                 SQ.entries[i].rsvd <= `SD 0;
             end 
-            oldest_unrsvd_sq_idx <=`SD 0;
-            sq_all_rsvd <=`SD 1;
+            secure_age <=`SD 0;
             forward_valid <= `SD 0;
         end
         else begin
-            sq_all_rsvd <=`SD (sq_empty || unkwn_idx==`SQ_CAPACITY);
-            oldest_unrsvd_sq_idx <=`SD unkwn_idx[`SQ_LEN-1:0];
+            secure_age <=`SD unkwn_idx;
 
             // To add the new instruction with unknown data and addr
             if (sq_enable) begin 
@@ -168,7 +182,17 @@ module store_queue (
                 SQ.entries[rs_sq_packet.sq_idx].PC <= `SD rs_sq_packet.PC;
             end
 
-            if (Dcache2SQ_response==SQ.entries[head].rob_idx) begin     // Retire
+            if (retire_enable&&SQ.entries[head].rsvd) begin     
+                // Retire SQ head and transfer it to Post-Retirement STORE Buffer
+                // When Store Buffer is non-empty, it sends packet to D$
+                // D$ will send store buffer packet to Memory when it is not busy
+                // Don't need to wait for D$ to finish this store to retire
+                // But when forwarding, load has to check STORE BUFFER TOO
+                sq2sb_packet.PC  <=`SD SQ.entries[head].PC; 
+                sq2sb_packet.addr  <=`SD SQ.entries[head].addr; 
+                sq2sb_packet.data  <=`SD SQ.entries[head].data; 
+                sq2sb_packet.store_byte  <=`SD SQ.entries[head].store_byte;                
+                // Retire the head
                 SQ.entries[head].rsvd <=`SD 0;
                 SQ.head <= `SD (SQ.head == `SQ_CAPACITY-1)? 0: SQ.head + 1;
             end
@@ -176,14 +200,19 @@ module store_queue (
             if (sq_enable && ~ Dcache2SQ_response)   counter <= `SD counter + 1;
             if (~sq_enable && Dcache2SQ_response)    counter <= `SD counter - 1;
             
-            if (match) begin
+            if (forward_match) begin
                 forward_valid <= `SD 1;
+                lb2cache_request_valid <= `SD 0;
                 forward_pack <= `SD 4ward;
             end
             else begin
                 forward_valid <= `SD 0;
+                lb2cache_request_valid <= `SD 1;
                 lb2cache_request_entry <=`SD lb2sq_request_entry;
             end
+
+            if (lb2cache_request_entry ==`SD lb2sq_request_entry)
+                lb2cache_request_valid <= `SD 0;
         end    
     end
 
