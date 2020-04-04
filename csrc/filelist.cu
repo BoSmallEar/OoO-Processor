9f25e4f2bb9ae6c7d158670073b1b96201d944ef
Shiyu Liu <shiyuliu@umich.edu>
2020-04-04 14:29:13 -0400

    debug_lsq_Apr_4

--- csrc/filelist.cu
+++ csrc/filelist.cu
@@ -1,12 +1,12 @@
 PIC_LD=ld
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _13495_archive_1.so
-_13495_archive_1.so : archive.3/_13495_archive_1.a
+ARCHIVE_OBJS += _4582_archive_1.so
+_4582_archive_1.so : archive.4/_4582_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -o .//../syn_simv.daidir//_13495_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -o .//../simv.daidir//_4582_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../syn_simv.daidir//_13495_archive_1.so $@
+	@ln -sf .//../simv.daidir//_4582_archive_1.so $@
 
 
 
@@ -16,8 +16,7 @@ _13495_archive_1.so : archive.3/_13495_archive_1.a
 %.o: %.c
 	$(CC_CG) $(CFLAGS_CG) -c -o $@ $<
 CU_UDP_OBJS = \
-objs/udps/hUcmi.o objs/udps/dKp3B.o objs/udps/F8ezs.o objs/udps/MzHq6.o objs/udps/PjGxs.o  \
-objs/udps/guAtk.o objs/udps/aKVa7.o objs/udps/GLrQJ.o 
+
 
 CU_LVL_OBJS = \
 SIM_l.o 
