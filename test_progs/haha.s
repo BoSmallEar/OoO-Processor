c68c286055566b86503048e75efa2fe79b5dbb0b
Shiyu Liu <shiyuliu@umich.edu>
2020-04-04 17:16:34 -0400

    debug_lsq_Apr_4_haha

--- test_progs/haha.s
+++ test_progs/haha.s
@@ -10,9 +10,11 @@ addi sp, sp, 4
 add a0, a2, a0
 add a0, a2, a1
 sw a0, 0(sp)
+lw a3, 0(sp)        #48
 addi sp, sp, -16
-lw a0, 0(sp)
-sub a0, a2, a0
+sw a3, 0(sp)        #56
+# lw a0, 0(sp)
+# sub a0, a2, a0
 nop
 nop
 wfi
