b354ca2e1f0b3bf35968c462c520f2d1c9da9737
Shiyu Liu <shiyuliu@umich.edu>
2020-04-06 00:59:12 -0400

    debug_lsq_fib_rec

--- DVEfiles/session.tcl
+++ DVEfiles/session.tcl
@@ -1,12 +1,13 @@
 # Begin_DVE_Session_Save_Info
 # DVE full session
-# Saved on Sun Apr 5 23:24:18 2020
+# Saved on Mon Apr 6 00:54:01 2020
 # Designs open: 1
 #   Sim: /afs/umich.edu/user/s/h/shiyuliu/Desktop/group8w20/dve
 # Toplevel windows open: 1
-# 	TopLevel.2
+# 	TopLevel.1
+#   Source.1: proc_testbench.print_rat.unnamed$$_0
 #   Group count = 1
-#   Group Group1 signal count = 6
+#   Group Group1 signal count = 19
 # End_DVE_Session_Save_Info
 
 # DVE version: N-2017.12-SP2-1_Full64
@@ -57,15 +58,15 @@ gui_src_preferences -tabstop 8 -maxbits 24 -windownumber 1
 # DVE top-level session
 
 
-# Create and position top-level window: TopLevel.2
+# Create and position top-level window: TopLevel.1
 
-if {![gui_exist_window -window TopLevel.2]} {
-    set TopLevel.2 [ gui_create_window -type TopLevel \
+if {![gui_exist_window -window TopLevel.1]} {
+    set TopLevel.1 [ gui_create_window -type TopLevel \
        -icon $::env(DVE)/auxx/gui/images/toolbars/dvewin.xpm] 
 } else { 
-    set TopLevel.2 TopLevel.2
+    set TopLevel.1 TopLevel.1
 }
-gui_show_window -window ${TopLevel.2} -show_state maximized -rect {{60 66} {2351 1238}}
+gui_show_window -window ${TopLevel.1} -show_state maximized -rect {{1 38} {2292 1210}}
 
 # ToolBar settings
 gui_set_toolbar_attributes -toolbar {TimeOperations} -dock_state top
@@ -112,16 +113,58 @@ gui_show_toolbar -toolbar {Testbench}
 # End ToolBar settings
 
 # Docked window settings
-gui_sync_global -id ${TopLevel.2} -option true
+set HSPane.1 [gui_create_window -type HSPane -parent ${TopLevel.1} -dock_state left -dock_on_new_line true -dock_extent 323]
+catch { set Hier.1 [gui_share_window -id ${HSPane.1} -type Hier] }
+gui_set_window_pref_key -window ${HSPane.1} -key dock_width -value_type integer -value 323
+gui_set_window_pref_key -window ${HSPane.1} -key dock_height -value_type integer -value -1
+gui_set_window_pref_key -window ${HSPane.1} -key dock_offset -value_type integer -value 0
+gui_update_layout -id ${HSPane.1} {{left 0} {top 0} {width 322} {height 707} {dock_state left} {dock_on_new_line true} {child_hier_colhier 211} {child_hier_coltype 107} {child_hier_colpd 0} {child_hier_col1 0} {child_hier_col2 1} {child_hier_col3 -1}}
+set DLPane.1 [gui_create_window -type DLPane -parent ${TopLevel.1} -dock_state left -dock_on_new_line true -dock_extent 369]
+catch { set Data.1 [gui_share_window -id ${DLPane.1} -type Data] }
+gui_set_window_pref_key -window ${DLPane.1} -key dock_width -value_type integer -value 369
+gui_set_window_pref_key -window ${DLPane.1} -key dock_height -value_type integer -value 706
+gui_set_window_pref_key -window ${DLPane.1} -key dock_offset -value_type integer -value 0
+gui_update_layout -id ${DLPane.1} {{left 0} {top 0} {width 368} {height 707} {dock_state left} {dock_on_new_line true} {child_data_colvariable 210} {child_data_colvalue 101} {child_data_coltype 52} {child_data_col1 0} {child_data_col2 1} {child_data_col3 2}}
+set Console.1 [gui_create_window -type Console -parent ${TopLevel.1} -dock_state bottom -dock_on_new_line true -dock_extent 391]
+gui_set_window_pref_key -window ${Console.1} -key dock_width -value_type integer -value 2232
+gui_set_window_pref_key -window ${Console.1} -key dock_height -value_type integer -value 391
+gui_set_window_pref_key -window ${Console.1} -key dock_offset -value_type integer -value 0
+gui_update_layout -id ${Console.1} {{left 0} {top 0} {width 2291} {height 390} {dock_state bottom} {dock_on_new_line true}}
+#### Start - Readjusting docked view's offset / size
+set dockAreaList { top left right bottom }
+foreach dockArea $dockAreaList {
+  set viewList [gui_ekki_get_window_ids -active_parent -dock_area $dockArea]
+  foreach view $viewList {
+      if {[lsearch -exact [gui_get_window_pref_keys -window $view] dock_width] != -1} {
+        set dockWidth [gui_get_window_pref_value -window $view -key dock_width]
+        set dockHeight [gui_get_window_pref_value -window $view -key dock_height]
+        set offset [gui_get_window_pref_value -window $view -key dock_offset]
+        if { [string equal "top" $dockArea] || [string equal "bottom" $dockArea]} {
+          gui_set_window_attributes -window $view -dock_offset $offset -width $dockWidth
+        } else {
+          gui_set_window_attributes -window $view -dock_offset $offset -height $dockHeight
+        }
+      }
+  }
+}
+#### End - Readjusting docked view's offset / size
+gui_sync_global -id ${TopLevel.1} -option true
 
-gui_set_env TOPLEVELS::TARGET_FRAME(Source) none
-gui_set_env TOPLEVELS::TARGET_FRAME(Schematic) none
-gui_set_env TOPLEVELS::TARGET_FRAME(PathSchematic) none
+# MDI window settings
+set Source.1 [gui_create_window -type {Source}  -parent ${TopLevel.1}]
+gui_show_window -window ${Source.1} -show_state maximized
+gui_update_layout -id ${Source.1} {{show_state maximized} {dock_state undocked} {dock_on_new_line false}}
+
+# End MDI window settings
+
+gui_set_env TOPLEVELS::TARGET_FRAME(Source) ${TopLevel.1}
+gui_set_env TOPLEVELS::TARGET_FRAME(Schematic) ${TopLevel.1}
+gui_set_env TOPLEVELS::TARGET_FRAME(PathSchematic) ${TopLevel.1}
 gui_set_env TOPLEVELS::TARGET_FRAME(Wave) none
 gui_set_env TOPLEVELS::TARGET_FRAME(List) none
-gui_set_env TOPLEVELS::TARGET_FRAME(Memory) none
+gui_set_env TOPLEVELS::TARGET_FRAME(Memory) ${TopLevel.1}
 gui_set_env TOPLEVELS::TARGET_FRAME(DriverLoad) none
-gui_update_statusbar_target_frame ${TopLevel.2}
+gui_update_statusbar_target_frame ${TopLevel.1}
 
 #</WindowLayout>
 
@@ -162,7 +205,7 @@ set _session_group_1 Group1
 gui_sg_create "$_session_group_1"
 set Group1 "$_session_group_1"
 
-gui_sg_addsignal -group "$_session_group_1" { proc_testbench.processor0.top_level0.dcache0.dcache_PC proc_testbench.processor0.top_level0.dcache0.dcache_valid proc_testbench.processor0.top_level0.dcache0.dcache_value proc_testbench.processor0.top_level0.dcache0.dcache_prf_idx proc_testbench.processor0.top_level0.dcache0.dcache_rob_idx proc_testbench.processor0.top_level0.dcache0.dcache_blocks }
+gui_sg_addsignal -group "$_session_group_1" { proc_testbench.processor0.top_level0.dcache0.clock proc_testbench.processor0.top_level0.dcache0.reset proc_testbench.processor0.top_level0.dcache0.dcache_PC proc_testbench.processor0.top_level0.dcache0.dcache_valid proc_testbench.processor0.top_level0.dcache0.dcache_value proc_testbench.processor0.top_level0.dcache0.dcache_prf_idx proc_testbench.processor0.top_level0.dcache0.dcache_rob_idx proc_testbench.processor0.top_level0.dcache0.dcache_blocks proc_testbench.processor0.top_level0.dcache0.Dcache2mem_command proc_testbench.processor0.top_level0.dcache0.Dcache2mem_addr proc_testbench.processor0.top_level0.dcache0.Dcache2mem_size proc_testbench.processor0.top_level0.dcache0.Dcache2mem_data proc_testbench.processor0.top_level0.dcache0.mem2Dcache_data proc_testbench.processor0.top_level0.dcache0.mem2Dcache_tag proc_testbench.processor0.top_level0.dcache0.mem2Dcache_response_valid proc_testbench.processor0.top_level0.dcache0.mem2Dcache_response proc_testbench.processor0.top_level0.dcache0.store_cache_hit proc_testbench.processor0.top_level0.dcache0.store_cache_hit_set proc_testbench.processor0.top_level0.dcache0.store_cache_hit_way }
 gui_set_radix -radix {decimal} -signals {Sim:proc_testbench.processor0.top_level0.dcache0.dcache_PC}
 gui_set_radix -radix {unsigned} -signals {Sim:proc_testbench.processor0.top_level0.dcache0.dcache_PC}
 gui_set_radix -radix {decimal} -signals {Sim:proc_testbench.processor0.top_level0.dcache0.dcache_value}
@@ -173,6 +216,22 @@ gui_set_radix -radix {decimal} -signals {Sim:proc_testbench.processor0.top_level
 gui_set_radix -radix {unsigned} -signals {Sim:proc_testbench.processor0.top_level0.dcache0.dcache_rob_idx}
 gui_set_radix -radix {decimal} -signals {Sim:proc_testbench.processor0.top_level0.dcache0.dcache_blocks}
 gui_set_radix -radix {unsigned} -signals {Sim:proc_testbench.processor0.top_level0.dcache0.dcache_blocks}
+gui_set_radix -radix enum -signals {Sim:proc_testbench.processor0.top_level0.dcache0.Dcache2mem_command}
+gui_set_radix -radix {decimal} -signals {Sim:proc_testbench.processor0.top_level0.dcache0.Dcache2mem_addr}
+gui_set_radix -radix {unsigned} -signals {Sim:proc_testbench.processor0.top_level0.dcache0.Dcache2mem_addr}
+gui_set_radix -radix enum -signals {Sim:proc_testbench.processor0.top_level0.dcache0.Dcache2mem_size}
+gui_set_radix -radix {decimal} -signals {Sim:proc_testbench.processor0.top_level0.dcache0.Dcache2mem_data}
+gui_set_radix -radix {unsigned} -signals {Sim:proc_testbench.processor0.top_level0.dcache0.Dcache2mem_data}
+gui_set_radix -radix {decimal} -signals {Sim:proc_testbench.processor0.top_level0.dcache0.mem2Dcache_data}
+gui_set_radix -radix {unsigned} -signals {Sim:proc_testbench.processor0.top_level0.dcache0.mem2Dcache_data}
+gui_set_radix -radix {decimal} -signals {Sim:proc_testbench.processor0.top_level0.dcache0.mem2Dcache_tag}
+gui_set_radix -radix {unsigned} -signals {Sim:proc_testbench.processor0.top_level0.dcache0.mem2Dcache_tag}
+gui_set_radix -radix {decimal} -signals {Sim:proc_testbench.processor0.top_level0.dcache0.mem2Dcache_response}
+gui_set_radix -radix {unsigned} -signals {Sim:proc_testbench.processor0.top_level0.dcache0.mem2Dcache_response}
+gui_set_radix -radix {decimal} -signals {Sim:proc_testbench.processor0.top_level0.dcache0.store_cache_hit_set}
+gui_set_radix -radix {unsigned} -signals {Sim:proc_testbench.processor0.top_level0.dcache0.store_cache_hit_set}
+gui_set_radix -radix {decimal} -signals {Sim:proc_testbench.processor0.top_level0.dcache0.store_cache_hit_way}
+gui_set_radix -radix {unsigned} -signals {Sim:proc_testbench.processor0.top_level0.dcache0.store_cache_hit_way}
 
 # Global: Highlighting
 
@@ -182,7 +241,7 @@ gui_change_stack_mode -mode list
 # Post database loading setting...
 
 # Restore C1 time
-gui_set_time -C1_only 24890
+gui_set_time -C1_only 31315
 
 
 
@@ -200,10 +259,45 @@ foreach __top [gui_ekki_get_window_ids -type TopLevel] {
 gui_set_loading_session_type noSession
 # DVE View/pane content session: 
 
+
+# Hier 'Hier.1'
+gui_show_window -window ${Hier.1}
+gui_list_set_filter -id ${Hier.1} -list { {Package 1} {All 0} {Process 1} {VirtPowSwitch 0} {UnnamedProcess 1} {UDP 0} {Function 1} {Block 1} {SrsnAndSpaCell 0} {OVA Unit 1} {LeafScCell 1} {LeafVlgCell 1} {Interface 1} {LeafVhdCell 1} {$unit 1} {NamedBlock 1} {Task 1} {VlgPackage 1} {ClassDef 1} {VirtIsoCell 0} }
+gui_list_set_filter -id ${Hier.1} -text {*}
+gui_hier_list_init -id ${Hier.1}
+gui_change_design -id ${Hier.1} -design Sim
+catch {gui_list_expand -id ${Hier.1} proc_testbench}
+catch {gui_list_expand -id ${Hier.1} proc_testbench.processor0}
+catch {gui_list_expand -id ${Hier.1} proc_testbench.processor0.top_level0}
+catch {gui_list_select -id ${Hier.1} {proc_testbench.processor0.top_level0.dcache0}}
+gui_view_scroll -id ${Hier.1} -vertical -set 83
+gui_view_scroll -id ${Hier.1} -horizontal -set 0
+
+# Data 'Data.1'
+gui_list_set_filter -id ${Data.1} -list { {Buffer 1} {Input 1} {Others 1} {Linkage 1} {Output 1} {LowPower 1} {Parameter 1} {All 1} {Aggregate 1} {LibBaseMember 1} {Event 1} {Assertion 1} {Constant 1} {Interface 1} {BaseMembers 1} {Signal 1} {$unit 1} {Inout 1} {Variable 1} }
+gui_list_set_filter -id ${Data.1} -text {*}
+gui_list_show_data -id ${Data.1} {proc_testbench.processor0.top_level0.dcache0}
+gui_show_window -window ${Data.1}
+catch { gui_list_select -id ${Data.1} {proc_testbench.processor0.top_level0.dcache0.store_cache_hit proc_testbench.processor0.top_level0.dcache0.store_cache_hit_set proc_testbench.processor0.top_level0.dcache0.store_cache_hit_way }}
+gui_view_scroll -id ${Data.1} -vertical -set 0
+gui_view_scroll -id ${Data.1} -horizontal -set 0
+gui_view_scroll -id ${Hier.1} -vertical -set 83
+gui_view_scroll -id ${Hier.1} -horizontal -set 0
+
+# Source 'Source.1'
+gui_src_value_annotate -id ${Source.1} -switch false
+gui_set_env TOGGLE::VALUEANNOTATE 0
+gui_open_source -id ${Source.1}  -replace -active {proc_testbench.print_rat.unnamed$$_0} /afs/umich.edu/user/s/h/shiyuliu/Desktop/group8w20/testbench/proc_testbench.sv
+gui_src_value_annotate -id ${Source.1} -switch true
+gui_set_env TOGGLE::VALUEANNOTATE 1
+gui_view_scroll -id ${Source.1} -vertical -set 4650
+gui_src_set_reusable -id ${Source.1}
 # Restore toplevel window zorder
 # The toplevel window could be closed if it has no view/pane
-if {[gui_exist_window -window ${TopLevel.2}]} {
-	gui_set_active_window -window ${TopLevel.2}
+if {[gui_exist_window -window ${TopLevel.1}]} {
+	gui_set_active_window -window ${TopLevel.1}
+	gui_set_active_window -window ${Source.1}
+	gui_set_active_window -window ${DLPane.1}
 }
 #</Session>
 
