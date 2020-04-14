84168bf88cea97324178edc9496c735474bc1004
刘知正 <893292259@qq.com>
2020-04-14 16:32:49 -0400

    add victim cache to icache

--- verilog/icache.sv
+++ verilog/icache.sv
@@ -39,43 +39,71 @@ module icache(
 	output logic    [`XLEN-1:0]     Icache2mem_addr  // Address sent to memory
 );
     logic [3:0] num_block_prefetch;
-    assign num_block_prefetch = 1; 
+    assign num_block_prefetch = 7; 
     logic [`XLEN-1:0] last_addr;
     logic [7:0] curr_tag;
     logic [4:0] curr_idx;
+    logic [12:0] curr_victim_tag;
     logic [`XLEN-1:0] send_addr;
     logic [7:0] send_tag;
     logic [4:0] send_idx;
+    logic [12:0] send_victim_tag;
     logic [1:0]      send_addr_state;
     logic [1:0] send_addr_hit;//0-check icache 1-send request. receive respose. 2-receive data
     logic [3:0] send_mem_tag;
     logic [`XLEN-1:0] goal_addr; 
     logic             change_addr;
+    logic             curr_victim_cache_hit;
+    logic             curr_victim_cache_way;
+    logic             send_victim_cache_hit;
+    logic             send_victim_cache_way;
  
     assign goal_addr = {proc2Icache_addr[31:3],3'b0} + num_block_prefetch*8; 
     assign curr_tag = proc2Icache_addr[15:8];
+    assign curr_victim_tag = proc2Icache_addr[15:3];
     assign curr_idx = proc2Icache_addr[7:3];
     assign send_tag = send_addr[15:8];
+    assign send_victim_tag = send_addr[15:3];
     assign send_idx = send_addr[7:3];
-    assign send_addr_hit = (icache_blocks[send_idx].valid) && (icache_blocks[send_idx].tag == send_tag);
+    assign send_addr_hit = ((icache_blocks[send_idx].valid) && (icache_blocks[send_idx].tag == send_tag)) || send_victim_cache_hit;
     assign Icache2mem_addr = send_addr;
     assign Icache2mem_command = (send_addr_state == 1) ? BUS_LOAD : BUS_NONE;
-    assign Icache2proc_valid = (icache_blocks[curr_idx].valid) && (icache_blocks[curr_idx].tag == curr_tag); 
-    assign Icache2proc_data =  proc2Icache_addr[2]? icache_blocks[curr_idx].data[63:32]: icache_blocks[curr_idx].data[31:0];
+    assign Icache2proc_valid = ((icache_blocks[curr_idx].valid) && (icache_blocks[curr_idx].tag == curr_tag)) || curr_victim_cache_hit; 
+    assign Icache2proc_data =  curr_victim_cache_hit? (proc2Icache_addr[2]? victim_cache.victim_blocks[curr_victim_cache_way].data[63:32]:  victim_cache.victim_blocks[curr_victim_cache_way].data[31:0]) : (proc2Icache_addr[2]? icache_blocks[curr_idx].data[63:32]: icache_blocks[curr_idx].data[31:0]);
     assign change_addr = (proc2Icache_addr != last_addr) && (proc2Icache_addr != last_addr+4);
+
     ICACHE_BLOCK [31:0] icache_blocks;
+    VICTIM_CACHE victim_cache;
+    
+    always_comb begin
+        curr_victim_cache_hit = 0;
+        send_victim_cache_hit = 0;
+        for(int i = 0; i < 2; i++) begin
+            if (victim_cache.victim_blocks[i].valid && (victim_cache.victim_blocks[i].tag == curr_victim_tag)) begin 
+                curr_victim_cache_hit = 1;
+                curr_victim_cache_way = i;
+            end    
+            if (victim_cache.victim_blocks[i].valid && (victim_cache.victim_blocks[i].tag == send_victim_tag)) begin 
+                send_victim_cache_hit = 1;
+                send_victim_cache_way = i;
+            end
+        end
+    end
 
  
     // synopsys sync_set_reset "reset"
     always_ff @(posedge clock) begin
-        if (reset) begin
-            int i;
-            for (i=0; i<32; i++) begin
+        if (reset) begin 
+            for (int i=0; i<32; i++) begin
                 icache_blocks[i].valid <= `SD 1'b0;
             end
             send_addr <= `SD 0;
             last_addr <= `SD 0;
             send_addr_state <= `SD 0;
+            for(int i = 0; i < 2; i++) begin
+                victim_cache.victim_blocks[i].valid <= `SD 1'b0;  
+            end
+            victim_cache.lru <= `SD 1'b0;
         end
         else begin  
             last_addr <= `SD proc2Icache_addr;
@@ -89,6 +117,15 @@ module icache(
                     if (send_addr_hit) begin
                         send_addr <= `SD send_addr + 4'h8;
                         send_addr_state <= `SD 0;
+                        if (send_victim_cache_hit) begin
+                            icache_blocks[send_idx].data <= `SD victim_cache.victim_blocks[send_victim_cache_way].data;
+                            icache_blocks[send_idx].tag <= `SD send_tag;
+                            icache_blocks[send_idx].valid <= `SD  victim_cache.victim_blocks[send_victim_cache_way].valid;
+                            victim_cache.victim_blocks[send_victim_cache_way].data <=  `SD    icache_blocks[send_idx].data;
+                            victim_cache.victim_blocks[send_victim_cache_way].valid <=  `SD   icache_blocks[send_idx].valid ;
+                            victim_cache.victim_blocks[send_victim_cache_way].tag <=  `SD   {icache_blocks[send_idx].tag,send_idx};
+                            victim_cache.lru <= `SD ~send_victim_cache_way;
+                        end
                     end
                     else    send_addr_state <= `SD 1;
                 end
@@ -106,6 +143,10 @@ module icache(
                         icache_blocks[send_idx].tag <= `SD send_tag;
                         icache_blocks[send_idx].valid <= `SD 1'b1;
                         icache_blocks[send_idx].data <= `SD mem2Icache_data;
+                        victim_cache.victim_blocks[victim_cache.lru].data <=  `SD    icache_blocks[send_idx].data;
+                        victim_cache.victim_blocks[victim_cache.lru].valid <=  `SD   icache_blocks[send_idx].valid ;
+                        victim_cache.victim_blocks[victim_cache.lru].tag <=  `SD   {icache_blocks[send_idx].tag,send_idx};
+                        victim_cache.lru <= `SD ~victim_cache.lru;
                     end
                 end
             end
